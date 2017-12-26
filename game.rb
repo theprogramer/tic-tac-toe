fbcfae8064af6e6d3a8e1c3119b86b3e18354016
Thiago Miranda <theprogramer@theprogramer.com.br>
2017-12-26 17:17:15 -0200

    Moving board code to ./lib

--- game.rb
+++ game.rb
@@ -1,32 +1,25 @@
-class Board
-  # the nought marker
-  NOUGHT = 'O'.freeze
-  # the cross marker
-  CROSS = 'X'.freeze
-  # the board options
-  OPTIONS = %w(0 1 2 3 4 5 6 7 8).freeze
-  # horizontal line
-  LINE = '===+===+==='.freeze
-end
+require_relative './lib/board'
 
 class Game
+  attr_reader :board
   def initialize
-    @board = %w(0 1 2 3 4 5 6 7 8) #Board::OPTIONS
+    # @board = %w(0 1 2 3 4 5 6 7 8) #Board::OPTIONS
+    @board = Board.new
     @com = Board::CROSS
     @hum = Board::NOUGHT
   end
 
   def start_game
     # start by printing the board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n#{Board::LINE}\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n#{Board::LINE}\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+    board.draw
     puts "Enter [0-8]:"
     # loop through until the game was won or tied
-    until game_is_over(@board) || tie(@board)
+    until game_is_over? || tie?
       get_human_spot
-      if !game_is_over(@board) && !tie(@board)
+      if !game_is_over? && !tie?
         eval_board
       end
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n#{Board::LINE}\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n#{Board::LINE}\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+      board.draw
     end
     puts "Game over"
   end
@@ -35,8 +28,8 @@ class Game
     spot = nil
     until spot
       spot = gets.chomp.to_i
-      if @board[spot] != Board::CROSS && @board[spot] != Board::NOUGHT
-        @board[spot] = @hum
+      if board.options[spot] != Board::CROSS && board.options[spot] != Board::NOUGHT
+        board.options[spot] = @hum
       else
         spot = nil
       end
@@ -46,13 +39,13 @@ class Game
   def eval_board
     spot = nil
     until spot
-      if @board[4] == "4"
+      if board.options[4] == "4"
         spot = 4
-        @board[spot] = @com
+        board.options[spot] = @com
       else
-        spot = get_best_move(@board, @com)
-        if @board[spot] != Board::CROSS && @board[spot] != Board::NOUGHT
-          @board[spot] = @com
+        spot = get_best_move(board.options, @com)
+        if board.options[spot] != Board::CROSS && board.options[spot] != Board::NOUGHT
+          board.options[spot] = @com
         else
           spot = nil
         end
@@ -70,13 +63,13 @@ class Game
     end
     available_spaces.each do |as|
       board[as.to_i] = @com
-      if game_is_over(board)
+      if game_is_over?
         best_move = as.to_i
         board[as.to_i] = as
         return best_move
       else
         board[as.to_i] = @hum
-        if game_is_over(board)
+        if game_is_over?
           best_move = as.to_i
           board[as.to_i] = as
           return best_move
@@ -93,22 +86,13 @@ class Game
     end
   end
 
-  def game_is_over(b)
-
-    [b[0], b[1], b[2]].uniq.length == 1 ||
-    [b[3], b[4], b[5]].uniq.length == 1 ||
-    [b[6], b[7], b[8]].uniq.length == 1 ||
-    [b[0], b[3], b[6]].uniq.length == 1 ||
-    [b[1], b[4], b[7]].uniq.length == 1 ||
-    [b[2], b[5], b[8]].uniq.length == 1 ||
-    [b[0], b[4], b[8]].uniq.length == 1 ||
-    [b[2], b[4], b[6]].uniq.length == 1
+  def game_is_over?
+    board.game_is_over
   end
 
-  def tie(b)
-    b.all? { |s| s == Board::CROSS || s == Board::NOUGHT }
+  def tie?
+    board.tie
   end
-
 end
 
 game = Game.new
---
+++ lib/board.rb
@@ -0,0 +1,53 @@
+class Board
+  attr_reader :options
+
+  # the nought marker
+  NOUGHT = 'O'.freeze
+  # the cross marker
+  CROSS = 'X'.freeze
+  # the board options
+  OPTIONS = %w(0 1 2 3 4 5 6 7 8) #.freeze
+  # horizontal line
+  LINE = '===+===+==='.freeze
+
+  # Rows
+  TOP = [0, 1, 2].freeze
+  MIDDLE = [3, 4, 5].freeze
+  BOTTOM = [6, 7, 8].freeze
+  LEFT = [0, 3, 6].freeze
+  CENTER = [1, 4, 7].freeze
+  RIGHT = [2, 5, 8].freeze
+  SLASH = [6, 4, 2].freeze
+  BACKSLASH = [0, 4, 8].freeze
+
+  def initialize
+    @options = Board::OPTIONS
+  end
+
+  # Draw board
+  def draw
+    puts " #{@options[0]} | #{@options[1]} | #{@options[2]} \n#{Board::LINE}\n #{@options[3]} | #{@options[4]} | #{@options[5]} \n#{Board::LINE}\n #{@options[6]} | #{@options[7]} | #{@options[8]} \n"
+  end
+
+  # Check if is a row
+  def row?(opt = [])
+    options.values_at(*opt).uniq.length == 1
+  end
+
+  # Game is over
+  def game_is_over
+    row?(TOP) ||
+      row?(MIDDLE) ||
+      row?(BOTTOM) ||
+      row?(LEFT) ||
+      row?(CENTER) ||
+      row?(RIGHT) ||
+      row?(SLASH) ||
+      row?(BACKSLASH)
+  end
+
+  # Tie
+  def tie
+    options.all? { |s| s == Board::CROSS || s == Board::NOUGHT }
+  end
+end
